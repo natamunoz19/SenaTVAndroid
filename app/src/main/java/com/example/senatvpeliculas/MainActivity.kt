3fa928ed5bb1f8d83b0989295332e933367a225f
Natalia <angynm@unicauca.edu.co>
2024-12-31 11:53:47 -0500

    Cargue 3

--- app/src/main/java/com/example/senatvpeliculas/MainActivity.kt
+++ app/src/main/java/com/example/senatvpeliculas/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.senatvpeliculas
 
-import MovieAdapter
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.senatvpeliculas.adapters.MovieAdapter
 import com.example.senatvpeliculas.data.MovieData
 
 class MainActivity : AppCompatActivity() {
